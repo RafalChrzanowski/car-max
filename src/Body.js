f4c3fdb3ab2f04095e0bb2b2e62d136d32ab1fbb
Kszanu <rafalchrzanowski00@gmail.com>
2023-01-04 20:13:06 +0100

    1

---
+++ src/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Body() {
+    return (
+      <div class="App-Body">
+        <img src={``} className="card-img-top"  alt='auto'/>
+      <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+    );
+  }
+  
+  
